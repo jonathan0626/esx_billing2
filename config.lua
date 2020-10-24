09a368387a74932de8703fee6fb967931b6028fd
Jonathan Peng <67518082+jonathan0626@users.noreply.github.com>
2020-10-25 00:31:45 +0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+Config.Locale = 'en'
+
+--是否讓自動繳費會扣更多錢
+--Whether to make automatic payments will deduct more money, if not, just set the value to 1 .
+
+Config.paymorebill = 1
+
+--設定自動繳費時間
+Config.time = 10 --假設 time == 10 ，即代表早上10點到11點	
---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+  ['invoices'] = '帳單',
+  ['invoices_item'] = '$%s',
+  ['received_invoice'] = '你 ~r~收到~s~ 一張帳單',
+  ['paid_invoice'] = '你 ~g~支付~s~ 帳單 金額: ~r~$%s~s~',
+  ['received_payment'] = '你 ~g~收到~s~ 帳單的付款 ~r~$%s~s~',
+  ['player_not_online'] = '玩家並未登入',
+  ['no_money'] = '你沒有足夠的錢來支付帳單',
+  ['target_no_money'] = '玩家 ~r~沒有~s~ 足夠的錢來支付帳單!',
+}
